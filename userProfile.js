43e5a1832935c291ba2173f20ce64c740aa33b6f
alexamaegazo <alexamaegazo5@gmail.com>
2024-11-04 20:34:29 +0800

    Initial commit with userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,12 @@
+// Create a user profile object
+let userProfile = {
+    name: "Alexa Mae Gazo",
+    age: 19,
+    favoriteNumber: 18,
+    favoriteColors: ["black", "violet", "pink"]
+  };
+  
+  // Print the user profile object
+  console.log(userProfile);
+  
+  
